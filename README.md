a8c991618bdd79b809979a15cec5a5aa404d76b8
odymit <15167656+odymit@users.noreply.github.com>
2024-09-09 11:05:34 +0800

    update 1. update README.md 2. update results collection function 3. update results

---
+++ README.md
@@ -0,0 +1,7 @@
+# fl-compr
+
+## Install Dependencies
+```
+# need python=3.10
+pip install -r requirements.txt
+```
---
+++ collect_results.py
@@ -0,0 +1,18 @@
+import shutil
+from pathlib import Path
+
+result_dir = Path(__file__).parent / "results"
+if not result_dir.exists():
+    result_dir.mkdir()
+
+# collect *.csv
+for item in Path(__file__).parent.iterdir():
+    if not item.is_dir():
+        continue
+    if item.name in ["results", "dataset"]:
+        continue
+    for file in item.iterdir():
+        if file.suffix == ".csv":
+            shutil.copy(file, result_dir / file.name)
+        if "result" in file.name and file.suffix == ".json":
+            shutil.copy(file, result_dir / file.name)
